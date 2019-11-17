f84646099b2ab627a58e55a80d9365ec03713199
Alex Macdonald <alexmac22347@gmail.com>
2019-11-17 13:43:43 -0800

    NetMQ socket works on Windows Reduce resolution to 1024 and 1 eye only. Still insanely slow on client side (recieves frame roughly once per minute)

--- Assets/Scripts/Test360Camera.cs
+++ Assets/Scripts/Test360Camera.cs
@@ -1,16 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
+using FrameServer;
 
 public class Test360Camera : MonoBehaviour
 {
-
+    private FrameServer.FrameServer frameServer;
     private RenderTexture cubemapLeftEye;
-    private RenderTexture cubemapRightEye;
     private RenderTexture equirect;
-    public bool renderStereo = true;
     private Texture2D newTexture;
     public float stereoSeparation = 0.064f;
     private float time;
+    private const int DIM = 1024;
     Texture2D texture;
     byte[] array;
 
@@ -19,16 +19,15 @@ public class Test360Camera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        texture = new Texture2D(2048, 2048, TextureFormat.ARGB32, false);
-        cubemapLeftEye = new RenderTexture(2048, 2048, 24, RenderTextureFormat.ARGB32);
+        frameServer = new FrameServer.FrameServer();
+        texture = new Texture2D(DIM, DIM, TextureFormat.ARGB32, false);
+        cubemapLeftEye = new RenderTexture(DIM, DIM, 24, RenderTextureFormat.ARGB32);
         cubemapLeftEye.dimension = TextureDimension.Cube;
-        cubemapRightEye = new RenderTexture(2048, 2048, 24, RenderTextureFormat.ARGB32);
-        cubemapRightEye.dimension = TextureDimension.Cube;
         //equirect height should be twice the height of cubemap
-        equirect = new RenderTexture(2048, 2048*2, 24, RenderTextureFormat.ARGB32);
+        equirect = new RenderTexture(DIM, DIM, 24, RenderTextureFormat.ARGB32);
 
-        newTexture = new Texture2D(2048, 2048, TextureFormat.ARGB32, false);
-        texture = new Texture2D(2048, 2048, TextureFormat.ARGB32, false);
+        newTexture = new Texture2D(DIM, DIM, TextureFormat.ARGB32, false);
+        texture = new Texture2D(DIM, DIM, TextureFormat.ARGB32, false);
     }
 
     void LateUpdate()
@@ -45,44 +44,28 @@ public class Test360Camera : MonoBehaviour
             Debug.Log("stereo 360 capture node has no camera or parent camera");
         }
 
-        if (renderStereo)
-        {
-            cam.stereoSeparation = stereoSeparation;
-            cam.RenderToCubemap(cubemapLeftEye, 63, Camera.MonoOrStereoscopicEye.Left);
-            cam.RenderToCubemap(cubemapRightEye, 63, Camera.MonoOrStereoscopicEye.Right);
-        }
-        else
-        {
-            cam.RenderToCubemap(cubemapLeftEye, 63, Camera.MonoOrStereoscopicEye.Mono);
-        }
+        cam.RenderToCubemap(cubemapLeftEye, 63, Camera.MonoOrStereoscopicEye.Mono);
 
         //optional: convert cubemaps to equirect
 
         if (equirect == null)
             return;
 
-        if (renderStereo)
-        {
-            cubemapLeftEye.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Left);
-            cubemapRightEye.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Right);
-        }
-        else
-        {
-            cubemapLeftEye.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Mono);
-        }
-
-
+        cubemapLeftEye.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Mono);
 
         /** Take render texture to texture 2d and then to byte **/
-        //texture = toTexture2D(equirect);
         RenderTexture.active = equirect;
         texture.ReadPixels(new Rect(0, 0, equirect.width, equirect.height), 0, 0);
-        //texture.Apply();
         byte[] img = texture.GetRawTextureData(); 
-        //array = texture.EncodeToPNG();
+        frameServer.SendFrame(img);
         newTexture.LoadRawTextureData(img);
         newTexture.Apply();
         m.SetTexture("_MainTex", newTexture);
     }
 
+    void OnApplicationQuit()
+    {
+        frameServer.Cleanup();
+    }
+    
 }
