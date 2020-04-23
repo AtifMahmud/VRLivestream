7e56f603c77074d6af09a5361ee3ceaf22d4a495
Atif Mahmud <atifmahmud95@gmail.com>
2020-04-23 14:06:17 -0700

    Main Mall Demo added

---
+++ MainMallDemo/Assets/Scripts/TestSceneChage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System; 
+
+public class TestSceneChage : MonoBehaviour
+{
+
+    long time;
+      
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = DateTime.Now.Millisecond;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DateTime.Now.Millisecond - time > 10000)
+        {
+            SceneManager.LoadScene("SceneChangeTest", LoadSceneMode.Additive);
+        }
+    }
+}
